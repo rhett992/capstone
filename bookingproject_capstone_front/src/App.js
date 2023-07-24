4bbe3b1758007fbb73a3e53d89f5a02f57b44b95
Rhett Klaus Añora <rhettdaffonanora@gmail.com>
2023-07-25 05:03:30 +0800

    ABOUT

--- bookingproject_capstone_front/src/App.js
+++ bookingproject_capstone_front/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
-import Booking from './components/Booking/Booking';
+// import Booking from './components/Booking/Booking';
 import Login from './components/Login/Login';
 import ContactUs from './components/Contact/ContactUs';
 
@@ -14,7 +14,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
-      <Route path='/booking' element={<Booking />} />
+      {/* <Route path='/booking' element={<Booking />} /> */}
       <Route path='/login' element={<Login />} />
       <Route path='/contactus' element={<ContactUs />} />
       </Routes>
